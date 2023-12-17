3ad2d8a2fe6dfa1add20457813dffaf540f88299
Swetha Janardhana <SwethaJanardhana85@gmail.com>
2023-12-17 18:04:43 +0100

    Next page, Previous page, Show Answer implemented

--- index.js
+++
@@ -1,7 +0,0 @@
-const bookmark = document.querySelector('[data-js="bookmark"]');
-console.log("outside");
-console.log("checking");
-bookmark.addEventListener("click", () => {
-  console.log("inside");
-  bookmark.classList.add("card_bookmark--clicked ");
-});
